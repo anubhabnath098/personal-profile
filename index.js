b7bc5483545723f894abb57d05d0b0f50bc9e4ed
anubhabnath098 <152593435+anubhabnath098@users.noreply.github.com>
2024-01-31 22:44:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const btn=document.querySelectorAll(".get-in-touch");
 btn.forEach(button => {
     button.addEventListener("click", sendemail)
 });
+function about(){
+    window.open('./public/about.html');
+}
+const btn2=document.querySelectorAll(".learn-more");
+btn2.forEach(button => {
+    button.addEventListener("click",about);
+});
