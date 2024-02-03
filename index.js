d00bf80d0e029ca52ba2da3b63f90b063dff9c54
anubhabnath098 <152593435+anubhabnath098@users.noreply.github.com>
2024-02-03 16:08:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ btn.forEach(button => {
     button.addEventListener("click", sendemail)
 });
 function about(){
-    window.open('./public/about.html');
+    window.open('./extrafiles/ANUBHAB.pdf');
 }
 const btn2=document.querySelectorAll(".learn-more");
 btn2.forEach(button => {
     button.addEventListener("click",about);
 });
+function hobby1(){
+    window.open('https://www.businessinsider.com/guides/learning/best-selling-books-of-all-time?IR=T');
+}
+function hobby2(){
+    window.open('https://store.steampowered.com/');
+}
+function hobby3(){
+    window.open('https://www.imdb.com/list/ls055592025/');
+}
+
+document.querySelector(".hobby1").addEventListener("click",hobby1);
+document.querySelector(".hobby2").addEventListener("click",hobby2);
+document.querySelector(".hobby3").addEventListener("click",hobby3);
